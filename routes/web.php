b3a454974833e06252ac785dfcbfb093c0fe4c00
KillerHeman <sharmahcool5@gmail.com>
2022-05-17 15:53:27 +0530

    Backend Setup

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//Frontend Routes
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Admin Routes
+Route::get('/admin', [AuthController::class,'dashboard'] );
